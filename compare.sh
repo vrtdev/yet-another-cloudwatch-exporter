997a6e3b8bed5afd1fb04d1bd30ad5ef8207a909
Ben Bridts <ben.bridts@vrt.be>
2023-10-05 15:42:58 +0200

    Update script

--- compare.sh
+++ compare.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-set -x
+set -eux
 
-./yace &
+a='yace-main'
+b='yace'
+
+./${a} --listen-address 127.0.0.1:5005 --config.file config-min.yml &
 PID=$(ps aux | grep yace | grep -v grep | awk '{print $2}')
-sleep 30
-curl -s localhost:5000/metrics | sed 's/\}.*/}/' | tee a.out | wc -l
+sleep 10
+curl -s localhost:5005/metrics | sed 's/\}.*/}/' | tee a.out | wc -l
 kill $PID
 
-./yace -enable-feature=aws-sdk-v2,max-dimensions-associator,list-metrics-callback &
+./${b} --listen-address 127.0.0.1:5005 --config.file config-min.yml &
 PID=$(ps aux | grep yace | grep -v grep | awk '{print $2}')
-sleep 30
-curl -s localhost:5000/metrics | sed 's/\}.*/}/' | tee b.out | wc -l
+sleep 10
+curl -s localhost:5005/metrics | sed 's/\}.*/}/' | tee b.out | wc -l
 kill $PID
 
 diff a.out b.out
+
+grep --only-matching '^aws_apigateway_\w*' a.out | sort | uniq -c | sort -n > a-apigw.out
+grep --only-matching '^aws_apigateway_\w*' b.out | sort | uniq -c | sort -n > b-apigw.out
+
+diff a-apigw.out b-apigw.out
+wc -l a-apigw.out b-apigw.out
